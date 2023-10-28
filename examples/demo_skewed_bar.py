a2be435826ea2275ae020366c5c8b7438d811164
Jae-Joon Lee <lee.j.joon@gmail.com>
2023-10-28 18:04:52 +0900

    update demo_skewed_bar.py with reflection and also add step-by-step example

--- examples/demo_skewed_bar.py
+++ examples/demo_skewed_bar.py
@@ -20,90 +20,96 @@ penguin_means = {
     'Flipper Length': (189.95, 195.82, 217.19),
 }
 
+n_species = len(species)
 x = np.arange(len(species))  # the label locations
-width = 0.25  # the width of the bars
-multiplier = 0
+n = len(penguin_means)
 
-for attribute, measurement in penguin_means.items():
-    offset = (width+0.1) * multiplier
-    rects = ax.bar(x + offset, measurement, width, label=attribute)
+space_between_species = 0.3
+space_between_attributes = 0.05
+
+bar_width = (1 - space_between_species - (n-1) * space_between_attributes) / n
+
+offset_between_attributes = np.arange(n) * (bar_width + space_between_attributes)
+label_offset = (1 - space_between_species) * 0.5
+
+for o, (attribute, measurement) in zip(offset_between_attributes,
+                                       penguin_means.items()):
+    rects = ax.bar(x + o + bar_width*0.5,  # bar_width*0.5 is needed as the bar
+                                           # is centered at the given position.
+                   measurement, bar_width, label=attribute)
     ax.bar_label(rects, padding=3)
-    multiplier += 1
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Length (mm)')
 ax.set_title('Penguin attributes by species')
-ax.set_xticks(x + width, species)
-ax.legend(loc='upper left', ncols=3)
-ax.set_ylim(0, 250)
+ax.set_xticks(x + label_offset, species)
+ax.legend(loc='upper left', ncols=n)
 
-# we adjust x and ylim to make room for 3d effects.
-ax.set_xlim(-0.4, 2.8)
-ax.set_ylim(-10, 290)
+for i in range(n_species):
+    for z, p in enumerate(ax.patches[i::n_species]):
+        p.set_zorder(p.get_zorder() - z*0.01)
 
-# We will skew the bar and squeez it a bit along the x-direction
-from mpl_visual_context.patheffects import (Recenter, PostAffine,
-                                            Offset)
+ax.set_xlim(-0.4, 3.1)
+ax.set_ylim(-25, 290)
 
-tr_skew = PostAffine().skew_deg(0, 10).scale(0.8, 1)
-offset = Offset(0, 5)
-
-for i in range(3):
-    tr_recenter = Recenter(ax, i, 0, coords="data")
-    skew = tr_recenter | tr_skew | tr_recenter.restore()
-    for p in ax.patches[i::3]:
-        p.set_path_effects([skew])
-    for l in ax.lines[i::3]:
-        l.set_path_effects([skew])
-    for t in ax.texts[i::3]:
-        t.set_path_effects([skew | offset])
-
-# adjust the pe of bar labels.
-from mpl_visual_context.patheffects import (GCModify, FillColor, StrokeColor,
-                                            HLSModify)
-
-for t, p in zip(ax.texts, ax.patches):
-    skew = t.get_path_effects()[0]
-    color = p.get_fc()
-    t.set_path_effects([
-        (skew | StrokeColor(color)
-         | HLSModify(l=0.3) | GCModify(linewidth=4)),
-        skew | FillColor("w")])
-
-# add 3d effects
+import mpl_visual_context.patheffects as pe
+from mpl_visual_context.patheffects_image_effect import ImageClipboard
 from matplotlib.colors import LightSource
-
 from mpl_poormans_3d import Poormans3d, Poormans3dFace
+from mpl_visual_context.artist_helper import ArtistListWithPE
+from mpl_visual_context.patheffects_image_effect import ReflectionArtist
+
+def colored_thick_line(c):
+    "patheffect to make thick outline with given color with lightness set at 0.3"
+    return pe.GCModify(linewidth=4) | pe.StrokeColor(c) | pe.HLSModify(l=0.3)
+
+ic = ImageClipboard()
+ic_flipped = ImageClipboard()
+
+tr_skew = pe.PostAffine().skew_deg(0, 10)
 
 ls = LightSource(azdeg=170)
 
-p3d = Poormans3d(ls, (-20, 7), fraction=0.5)
-p3d_face = Poormans3dFace(ls, (0, 0), fraction=0.5)
+p3d = Poormans3d(ls, displacement=(-10, 7), fraction=0.5)
+p3d_face = Poormans3dFace(ls, displacement=(0, 0), fraction=0.5)
+rc = pe.RoundCorner(5)
 
+ic= ImageClipboard()
+ic_flipped = ImageClipboard()
 
-# we recent the path effects, now with 3d effect
-for i in range(3):
-    tr_recenter = Recenter(ax, i, 0, coords="data")
+for i in range(n_species):
+    tr_recenter = pe.Recenter(ax, i, 0, coords="data")
     skew = tr_recenter | tr_skew | tr_recenter.restore()
-    for z, p in enumerate(ax.patches[i::3]):
-        p.set_path_effects([skew | p3d,
-                            skew | p3d_face])
-        # One may use ArtistListWithPoormans3d, but simply adjusting zorder
-        # works in this case.
-        p.set_zorder(p.get_zorder() - z*0.01)
+    skew_flipped = (tr_recenter | pe.PostAffine().scale(1, -1)
+                    | tr_skew  | tr_recenter.restore() | pe.Offset(0, -5))
 
+    pe_reflection = [
+        rc | skew_flipped | p3d | ic_flipped.copy(),
+        rc | skew_flipped | p3d_face | ic_flipped.copy(),
+        rc | skew | p3d | ic.copy(),
+        rc | skew | p3d_face | ic.copy()
+    ]
 
-# round corner
-if True:
-    from mpl_visual_context.patheffects import (RoundCorner,)
-    rc = RoundCorner(15, i_selector=lambda i: i % 2)
-    p3d = Poormans3d(ls, (-10, 5), fraction=0.5)
-    p3d_face = Poormans3dFace(ls, (0, 0), fraction=0.5)
-    for i in range(3):
-        tr_recenter = Recenter(ax, i, 0, coords="data")
-        skew = tr_recenter | tr_skew | tr_recenter.restore()
-        for z, p in enumerate(ax.patches[i::3]):
-            p.set_path_effects([rc | skew | p3d,
-                                rc | skew | p3d_face])
+    a_flipped = ArtistListWithPE(ax.patches[i::n_species], pe_reflection)
+    a_flipped.set_zorder(0.5)
+    ax.add_artist(a_flipped)
+
+    for p, t in zip(ax.patches[i::n_species], ax.texts[i::n_species]):
+        p.set_path_effects([skew | rc | p3d,
+                            skew | rc | p3d_face,
+                            ])
+
+        t.set_path_effects([skew | colored_thick_line(p.get_fc()),
+                            skew | pe.FillColor("w")
+                            ])
+
+a = ReflectionArtist(ic_flipped, clipboard_alpha=ic,
+                     alpha_dist_sigma=20, # image will become transparent about
+                                          # 20 points away from the originl
+                                          # imge.
+                     clear_alpha=True # make sure that clipboard_alpha is cleared after use.
+                     )
+a.set_zorder(0.7)
+ax.add_artist(a)
 
 plt.show()
